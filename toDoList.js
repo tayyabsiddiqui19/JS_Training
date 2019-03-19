976f2d06b6d4b492194c455e105757be55e199db
tayyabsiddiqui19 <tayyabsiddiqui@live.com>
2019-03-19 10:51:29 +0500

    To Do List

---
+++ toDoList.js
@@ -0,0 +1,6 @@
+let todo = ['Go to Office', 'Learn JavaScript', 'Practice the Code', 'Move Forward']
+
+console.log(todo.length)
+console.log(`You have ${todo.length} todo's`)
+console.log(`First index is ${todo[0]}, Second index is ${todo[1]}, Last Index is ${todo[todo.length -1]}`)
+console.log(todo.indexOf("Learn JavaScript"))
